d784e11c51b83c82ba1ef8221ce25151aa23035b
Leon <dhimbaleon356@gmail.com>
2022-08-17 11:06:47 +0200

    initial project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+
+      <h1>React getting started</h1> 
+      <ExpenseItem></ExpenseItem>
+
+    </div>
+    
+  );
+}
+
+export default App;
