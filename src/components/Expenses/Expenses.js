f9270c9051abcebff2fcd53dbed92eaaf59068a0
Leon <dhimbaleon356@gmail.com>
2022-08-28 09:58:55 +0200

    adding key on list Items

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,9 +5,10 @@ import Card from "./Card";
 function Expenses(props) {
   return (
     <Card className="expenses">
-      
+
       {props.items.map((expense) => (
         <ExpenseItem
+          key = {expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
