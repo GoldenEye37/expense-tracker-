538c3c3bafeb32ec4de3275d7887a6e962ad07d2
Leon <dhimbaleon356@gmail.com>
2022-08-28 08:10:36 +0200

    rendering list of data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,35 +1,20 @@
-import '../UI/Expenses.css';
-import ExpenseItem from './ExpenseItem';
-import Card from './Card';
+import "../UI/Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
 function Expenses(props) {
-
   return (
     <Card className="expenses">
-
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      ></ExpenseItem>
+      
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 }
-
 
 export default Expenses;
